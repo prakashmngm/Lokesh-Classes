a1a445604778785b93368dfd72cb1a373b424fdd
prakashmngm <mangam.suryaprakash@gmail.com>
2021-02-25 09:44:35 +0530

    Add files via upload

---
+++ Swapping_Elements.py
@@ -0,0 +1,26 @@
+'''
+
+Problem : Swap Variables
+
+Input : a=5, b=10
+Output : a=10, b=5
+
+
+Pseudo Code : 
+1. Take a backup variable ‘c’
+2. Assign c with a
+3. Assign a with b
+4. Assign b with c
+
+'''
+
+a = 5
+b = 10
+print(a,b)
+c = a
+a = b
+b = c
+print(a,b)
+
+
+
